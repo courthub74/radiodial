38ebad862847cd9d730da6cf59a7d2c037ee1289
Courtney Jacobs <courtneyj3470@gmail.com>
2020-03-10 07:42:23 -0400

    Update radio.py

--- radio.py
+++ radio.py
@@ -4,7 +4,7 @@ from tkinter import ttk
 
 app = Tk()
 app.title("RadioDial")
-app.geometry("300x300")
+app.geometry("150x300")
 
 # Function get the StringVar
 
@@ -19,7 +19,7 @@ r1.grid(row=0, column=0)
 r2 = ttk.Radiobutton(app, text="2", value="Two", var=dials)
 r2.grid(row=1, column=0)
 
-r3 = ttk.Radiobutton(app, text="3", value="Three", var=dials)
+r3 = ttk.Radiobutton(app, text="3", value="Tree", var=dials)
 r3.grid(row=2, column=0)
 
 r4 = ttk.Radiobutton(app, text="4", value="Four", var=dials)
@@ -32,7 +32,7 @@ r5.grid(row=4, column=0)
 b1 = Button(app, text="Submit", command=radio)
 b1.grid(row=5, column=0)
 
-tfield = Text(app, width=20, height=5)
+tfield = Text(app, width=10, height=5, font="Arial 20 bold")
 tfield.grid(row=6, column=0)
 
 app.mainloop()
